9183edf74ba57634d79cee7cc2955d09bf65308b
eaelguea <31897884+eaelguea@users.noreply.github.com>
2018-12-06 09:09:14 -0700

    Update P5MinimumPathSum.java

--- Problems/P5MinimumPathSum.java
+++ Problems/P5MinimumPathSum.java
@@ -1,23 +1,20 @@
 
 public class P5MinimumPathSum {
     public static int minPathSum(int[][] grid){
-      int row = grid.length;
-		  int col = grid[0].length;
-      if(row==0||col==0)return 0;
-		  
-      //Set first row and first column of dp table
-		  for(int i =1;i<row;i++) {
-			  grid[i][0]=grid[i][0]+grid[i-1][0];
-		  }
-		  for(int j=1;j<col;j++) {
-			  grid[0][j]=grid[0][j]+grid[0][j-1];
-		  }
-		
-		  for(int i=1;i<row;i++) {
-			  for(int j=1;j<col;j++) {
-				  grid[i][j]=Math.min(grid[i-1][j]+grid[i][j], grid[i][j-1]+grid[i][j]);
-			  }
-		  }
-		  return grid[row-1][col-1];
-	  }
+      	int row = grid.length;
+	int col = grid[0].length;
+      	if(row==0||col==0)return 0;
+	for(int i =1;i<row;i++) {
+		grid[i][0]=grid[i][0]+grid[i-1][0];
+	}
+	for(int j=1;j<col;j++) {
+		grid[0][j]=grid[0][j]+grid[0][j-1];
+	}
+	for(int i=1;i<row;i++) {
+		for(int j=1;j<col;j++) {
+			grid[i][j]=Math.min(grid[i-1][j]+grid[i][j], grid[i][j-1]+grid[i][j]);
+		}
+	}
+	return grid[row-1][col-1];
+    }
 }
