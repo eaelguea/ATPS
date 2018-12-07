8912b8b49027847208978f1492840f49195f22cd
eaelguea <31897884+eaelguea@users.noreply.github.com>
2018-12-06 20:45:08 -0700

    Update P4IntToRoman.java

--- Problems/P4IntToRoman.java
+++ Problems/P4IntToRoman.java
@@ -1,14 +1,14 @@
 import java.util.*;
 
 public class P4IntToRoman {
-  public String intToRoman(int num) {
+  public String intToRoman(int n) {
     int[] nums = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
     String[] numerals = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
     
     StringBuilder sb = new StringBuilder();
     for(int i=0 i<nums.length; i++){
-      while(num >= nums[i]){
-        num -= nums[i];
+      while(n >= nums[i]){
+        n -= nums[i];
         sb.append(numerals[i]);
       }
     }
