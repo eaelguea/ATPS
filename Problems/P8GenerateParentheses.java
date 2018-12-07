cb674aee70c5f51fe1fe180566b3f782fd25f8d3
eaelguea <31897884+eaelguea@users.noreply.github.com>
2018-12-06 22:25:14 -0700

    Create P8GenerateParentheses.java

---
+++ Problems/P8GenerateParentheses.java
@@ -0,0 +1,17 @@
+public class P8GenerateParentheses {
+  public List<String> generateParenthesis(int n) {
+          List<String> l1 = new ArrayList();
+          generate(l1, "", 0, 0, n);
+          return l1;
+  }
+  public void generate(List<String> l1, String str, int a, int b, int x){
+        if (str.length() == x * 2) {
+            l1.add(str);
+            return;
+        }
+        if (a < x)
+            generate(l1, str+"(", a+1, b, x);
+        if (b < a)
+            generate(l1, str+")", a, b+1, x);
+    }
+}
